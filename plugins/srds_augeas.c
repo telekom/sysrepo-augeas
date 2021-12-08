2e04e6cd565474d9a57c39dd895f6f3fb127fc23
Michal Vasko <mvasko@cesnet.cz>
2021-12-08 16:22:25 +0100

    srds augeas FEATURE basic store functionality
    With tests, which were overall improved.

--- plugins/srds_augeas.c
+++ plugins/srds_augeas.c
@@ -46,6 +46,14 @@ enum augds_ext_node_type {
     AUGDS_EXT_NODE_LABEL_VALUE  /**< matches any augeas label with value being the label, starts with '$$' */
 };
 
+enum augds_diff_op {
+    AUGDS_OP_UNKNOWN = 0,
+    AUGDS_OP_CREATE,
+    AUGDS_OP_DELETE,
+    AUGDS_OP_REPLACE,
+    AUGDS_OP_NONE
+};
+
 static struct auginfo {
     augeas *aug;    /**< augeas handle */
 
@@ -96,6 +104,29 @@ augds_get_lens(const struct lys_module *mod)
 }
 
 /**
+ * @brief Get last path segment (node) from a path.
+ *
+ * @param[in] path Augeas path.
+ * @param[in] skip_special_chars Whether to skip special characters at the beginning of the node.
+ * @return Last node.
+ */
+static const char *
+augds_get_path_node(const char *path, int skip_special_chars)
+{
+    const char *ptr;
+
+    /* get last path segment */
+    ptr = strrchr(path, '/');
+    ptr = ptr ? ptr + 1 : path;
+
+    if (skip_special_chars && (!strncmp(ptr, "$$", 2) || !strncmp(ptr, "##", 2))) {
+        ptr += 2;
+    }
+
+    return ptr;
+}
+
+/**
  * @brief Check for augeas errors.
  *
  * @param[in] aug Augeas handle.
@@ -104,31 +135,69 @@ augds_get_lens(const struct lys_module *mod)
 static int
 augds_check_erraug(augeas *aug)
 {
-    const char *aug_err_mmsg, *aug_err_details;
+    int rc = SR_ERR_OK, i, label_count = 0;
+    const char *aug_err_mmsg, *aug_err_details, *data_error, *data_error_msg;
+    char **label_matches = NULL;
 
     if (!aug) {
         SRPLG_LOG_ERR(srpds_name, "Augeas init failed.");
         return SR_ERR_OPERATION_FAILED;
     }
 
-    if (aug_error(aug) == AUG_NOERROR) {
-        /* no error */
-        return SR_ERR_OK;
-    }
-
     if (aug_error(aug) == AUG_ENOMEM) {
         /* memory error */
         AUG_LOG_ERRMEM;
         return SR_ERR_NO_MEMORY;
+    } else if (aug_error(aug) != AUG_NOERROR) {
+        /* complex augeas error */
+        aug_err_mmsg = aug_error_minor_message(aug);
+        aug_err_details = aug_error_details(aug);
+        SRPLG_LOG_ERR(srpds_name, "Augeas init failed (%s%s%s%s%s).", aug_error_message(aug),
+                aug_err_mmsg ? "; " : "", aug_err_mmsg ? aug_err_mmsg : "", aug_err_details ? "; " : "",
+                aug_err_details ? aug_err_details : "");
+        return SR_ERR_OPERATION_FAILED;
     }
 
-    /* complex augeas error */
-    aug_err_mmsg = aug_error_minor_message(aug);
-    aug_err_details = aug_error_details(aug);
-    SRPLG_LOG_ERR(srpds_name, "Augeas init failed (%s%s%s%s%s).", aug_error_message(aug),
-            aug_err_mmsg ? "; " : "", aug_err_mmsg ? aug_err_mmsg : "", aug_err_details ? "; " : "",
-            aug_err_details ? aug_err_details : "");
-    return SR_ERR_OPERATION_FAILED;
+    /* check for data error */
+    label_count = aug_match(aug, "/augeas/files//error//.", &label_matches);
+    if (label_count == -1) {
+        AUG_LOG_ERRAUG_GOTO(aug, rc, cleanup);
+    } else if (!label_count) {
+        /* no error */
+        goto cleanup;
+    }
+
+    /* data error */
+    assert(!strcmp(augds_get_path_node(label_matches[0], 0), "error"));
+    if (aug_get(aug, label_matches[0], &data_error) != 1) {
+        AUG_LOG_ERRAUG_GOTO(aug, rc, cleanup);
+    }
+
+    /* data error message */
+    data_error_msg = NULL;
+    for (i = 1; i < label_count; ++i) {
+        if (!strcmp(augds_get_path_node(label_matches[i], 0), "message")) {
+            if (aug_get(aug, label_matches[i], &data_error_msg) != 1) {
+                AUG_LOG_ERRAUG_GOTO(aug, rc, cleanup);
+            }
+            break;
+        }
+    }
+
+    assert(data_error);
+    if (data_error_msg) {
+        SRPLG_LOG_ERR(srpds_name, "Augeas data error \"%s\" (%s).", data_error, data_error_msg);
+    } else {
+        SRPLG_LOG_ERR(srpds_name, "Augeas data error \"%s\".", data_error);
+    }
+    rc = SR_ERR_OPERATION_FAILED;
+
+cleanup:
+    for (i = 0; i < label_count; ++i) {
+        free(label_matches[i]);
+    }
+    free(label_matches);
+    return rc;
 }
 
 /**
@@ -181,22 +250,6 @@ augds_get_ext_path(const struct lysc_node *node)
     }
 
     return NULL;
-}
-
-/**
- * @brief Get last path segment (node) from a path.
- *
- * @param[in] path Augeas path.
- * @return Last node.
- */
-static const char *
-augds_get_path_node(const char *path)
-{
-    const char *ptr;
-
-    /* find last path segment */
-    ptr = strrchr(path, '/');
-    return ptr ? ptr + 1 : path;
 }
 
 /**
@@ -298,7 +351,7 @@ srpds_aug_init(const struct lys_module *mod, sr_datastore_t ds, const char *owne
     }
 
     /* init augeas with all modules but no loaded files */
-    auginfo.aug = aug_init(NULL, NULL, AUG_NO_LOAD | AUG_NO_ERR_CLOSE);
+    auginfo.aug = aug_init(NULL, NULL, AUG_NO_LOAD | AUG_NO_ERR_CLOSE | AUG_SAVE_BACKUP);
     if ((rc = augds_check_erraug(auginfo.aug))) {
         goto cleanup;
     }
@@ -322,6 +375,11 @@ srpds_aug_init(const struct lys_module *mod, sr_datastore_t ds, const char *owne
         AUG_LOG_ERRMEM_GOTO(rc, cleanup);
     }
     aug_rm(auginfo.aug, path);
+
+    /* create new file instead of creating a backup and overwriting */
+    if (aug_set(auginfo.aug, "/augeas/save", "newfile") == -1) {
+        AUG_LOG_ERRAUG_GOTO(auginfo.aug, rc, cleanup);
+    }
 
     /* set only this single test file to be loaded */
     if (aug_set(auginfo.aug, path, AUG_TEST_INPUT_FILE) == -1) {
@@ -372,11 +430,297 @@ srpds_aug_destroy(const struct lys_module *mod, sr_datastore_t ds)
     return SR_ERR_OK;
 }
 
+static enum augds_diff_op
+augds_diff_str2op(const char *str)
+{
+    switch (str[0]) {
+    case 'c':
+        assert(!strcmp(str, "create"));
+        return AUGDS_OP_CREATE;
+    case 'd':
+        assert(!strcmp(str, "delete"));
+        return AUGDS_OP_DELETE;
+    case 'r':
+        assert(!strcmp(str, "replace"));
+        return AUGDS_OP_REPLACE;
+    case 'n':
+        assert(!strcmp(str, "none"));
+        return AUGDS_OP_NONE;
+    }
+
+    AUG_LOG_ERRINT;
+    return 0;
+}
+
+/**
+ * @brief Learn operation of a diff node.
+ *
+ * @param[in] diff_node Diff node.
+ * @return Diff node operation.
+ * @return 0 if no operation is defined on the node.
+ */
+static enum augds_diff_op
+augds_diff_get_op(const struct lyd_node *diff_node)
+{
+    struct lyd_meta *meta;
+
+    LY_LIST_FOR(diff_node->meta, meta) {
+        if (!strcmp(meta->name, "operation") && !strcmp(meta->annotation->module->name, "yang")) {
+            return augds_diff_str2op(lyd_get_meta_value(meta));
+        }
+    }
+
+    return 0;
+}
+
+static const char *
+augds_strchr_back(const char *start, const char *s, int c)
+{
+    const char *ptr;
+
+    for (ptr = s; ptr[0] != c; --ptr) {
+        if (ptr == start) {
+            return NULL;
+        }
+    }
+
+    return ptr;
+}
+
+static int
+augds_node_yang2aug(const struct lyd_node *node, const char *parent_aug_path, char **aug_path, const char **aug_value)
+{
+    int rc = SR_ERR_OK, path_seg_len;
+    const char *ext_path, *parent_ext_path, *parent_node, *start, *end, *node_name, *path_segment;
+    char *result = NULL, *tmp;
+    enum augds_ext_node_type node_type;
+
+    /* node extension data-path */
+    ext_path = augds_get_ext_path(node->schema);
+    if (!ext_path) {
+        /* nothing to set in augeas data */
+        *aug_path = NULL;
+        *aug_value = NULL;
+        goto cleanup;
+    }
+
+    /* parent last node */
+    parent_ext_path = node->parent ? augds_get_ext_path(node->parent->schema) : NULL;
+    parent_node = parent_ext_path ? augds_get_path_node(parent_ext_path, 1) : NULL;
+
+    end = ext_path + strlen(ext_path);
+    do {
+        start = augds_strchr_back(ext_path, end - 1, '/');
+        if (start) {
+            /* skip slash */
+            ++start;
+        } else {
+            /* first node, last processed */
+            start = ext_path;
+        }
+
+        /* handle special ext path node characters */
+        if (!strncmp(start, "$$", 2)) {
+            node_type = AUGDS_EXT_NODE_LABEL_VALUE;
+            node_name = start + 2;
+        } else if (!strncmp(start, "##", 2)) {
+            node_type = AUGDS_EXT_NODE_POSITION;
+            node_name = start + 2;
+        } else {
+            node_type = AUGDS_EXT_NODE_VALUE;
+            node_name = start;
+        }
+
+        if (parent_node && ((signed)strlen(parent_node) == end - node_name) &&
+                !strncmp(parent_node, node_name, strlen(parent_node))) {
+            /* remaining ext path prefix is the ext path of the parent */
+            assert(parent_aug_path);
+            path_segment = parent_aug_path;
+            path_seg_len = strlen(path_segment);
+
+            /* full path processed, exit loop */
+            start = ext_path;
+        } else {
+            switch (node_type) {
+            case AUGDS_EXT_NODE_LABEL_VALUE:
+                /* YANG data value as augeas label */
+                if (node->schema->nodetype == LYS_LIST) {
+                    assert(lyd_child(node)->schema->flags & LYS_KEY);
+                    path_segment = lyd_get_value(lyd_child(node));
+                } else {
+                    path_segment = lyd_get_value(node);
+                }
+                path_seg_len = strlen(path_segment);
+                break;
+            case AUGDS_EXT_NODE_POSITION:
+            case AUGDS_EXT_NODE_VALUE:
+                /* ext path node (YANG schema node name) as augeas label */
+                path_segment = node_name;
+                path_seg_len = end - node_name;
+                break;
+            }
+        }
+
+        if (!result) {
+            /* last node being processed (first iteration), set augeas value */
+            switch (node_type) {
+            case AUGDS_EXT_NODE_LABEL_VALUE:
+            case AUGDS_EXT_NODE_POSITION:
+                *aug_value = NULL;
+                break;
+            case AUGDS_EXT_NODE_VALUE:
+                if (node->schema->nodetype == LYS_LIST) {
+                    assert(lyd_child(node)->flags & LYS_KEY);
+                    *aug_value = lyd_get_value(lyd_child(node));
+                } else {
+                    *aug_value = lyd_get_value(node);
+                }
+                break;
+            }
+        }
+
+        /* add path segment */
+        if (asprintf(&tmp, "%.*s%s%s", path_seg_len, path_segment, result ? "/" : "", result ? result : "") == -1) {
+            AUG_LOG_ERRMEM_GOTO(rc, cleanup);
+        }
+        free(result);
+        result = tmp;
+
+        /* next iter */
+        end = start - 1;
+    } while (start != ext_path);
+
+cleanup:
+    if (rc) {
+        free(result);
+        *aug_path = NULL;
+    } else {
+        *aug_path = result;
+    }
+    return rc;
+}
+
+static int
+augds_yang2aug_diff_siblings_r(augeas *aug, const struct lyd_node *diff, const char *parent_path,
+        enum augds_diff_op parent_op)
+{
+    int rc = SR_ERR_OK;
+    enum augds_diff_op cur_op;
+    char *aug_path = NULL;
+    const char *aug_value;
+
+    LY_LIST_FOR(diff, diff) {
+        /* generate augeas path */
+        if ((rc = augds_node_yang2aug(diff, parent_path, &aug_path, &aug_value))) {
+            goto cleanup;
+        }
+
+        /* get node operation */
+        cur_op = augds_diff_get_op(diff);
+        if (!cur_op) {
+            cur_op = parent_op;
+        }
+        assert(cur_op);
+
+        if (aug_path) {
+            switch (cur_op) {
+            case AUGDS_OP_CREATE:
+            case AUGDS_OP_REPLACE:
+                /* set the augeas data */
+                if (aug_set(aug, aug_path, aug_value) == -1) {
+                    AUG_LOG_ERRAUG_GOTO(aug, rc, cleanup);
+                }
+                break;
+            case AUGDS_OP_DELETE:
+                /* remove the augeas data */
+                if (aug_rm(aug, aug_path) == 0) {
+                    AUG_LOG_ERRINT_GOTO(rc, cleanup);
+                }
+
+                /* all descendants were deleted, too */
+                continue;
+            case AUGDS_OP_NONE:
+                /* nothing to do */
+                break;
+            case AUGDS_OP_UNKNOWN:
+                AUG_LOG_ERRINT_RET;
+            }
+        } /* else no corresponding augeas data for the YANG node */
+
+        /* process children recursively */
+        if ((rc = augds_yang2aug_diff_siblings_r(aug, lyd_child_no_keys(diff), aug_path, cur_op))) {
+            goto cleanup;
+        }
+
+        free(aug_path);
+        aug_path = NULL;
+    }
+
+cleanup:
+    free(aug_path);
+    return rc;
+}
+
+static int srpds_aug_load(const struct lys_module *mod, sr_datastore_t ds, const char **xpaths, uint32_t xpath_count,
+        struct lyd_node **mod_data);
+
 static int
 srpds_aug_store(const struct lys_module *mod, sr_datastore_t ds, const struct lyd_node *mod_data)
 {
+    int rc = SR_ERR_OK, i, label_count;
+    struct lyd_node *cur_data = NULL, *diff = NULL;
+    char *lens_name = NULL, *path = NULL, **label_matches = NULL;
+    const char *value;
+
     /* get current data */
-    /* TODO */
+    if ((rc = srpds_aug_load(mod, ds, NULL, 0, &cur_data))) {
+        goto cleanup;
+    }
+
+    /* get diff with the updated data */
+    if (lyd_diff_siblings(cur_data, mod_data, 0, &diff)) {
+        AUG_LOG_ERRLY_GOTO(mod->ctx, rc, cleanup);
+    }
+
+    /* set augeas context for relative paths */
+    lens_name = augds_get_lens(mod);
+    if (asprintf(&path, "/augeas/files//*[lens='@%s']/path", lens_name) == -1) {
+        AUG_LOG_ERRMEM_GOTO(rc, cleanup);
+    }
+    label_count = aug_match(auginfo.aug, path, &label_matches);
+    if (label_count == -1) {
+        AUG_LOG_ERRAUG_GOTO(auginfo.aug, rc, cleanup);
+    } else if (!label_count) {
+        AUG_LOG_ERRINT_GOTO(rc, cleanup);
+    }
+    /* TODO what about the other files? */
+    if (aug_get(auginfo.aug, label_matches[0], &value) != 1) {
+        AUG_LOG_ERRAUG_GOTO(auginfo.aug, rc, cleanup);
+    }
+    if (aug_set(auginfo.aug, "/augeas/context", value) == -1) {
+        AUG_LOG_ERRAUG_GOTO(auginfo.aug, rc, cleanup);
+    }
+
+    /* apply diff to augeas data */
+    if ((rc = augds_yang2aug_diff_siblings_r(auginfo.aug, diff, NULL, 0))) {
+        goto cleanup;
+    }
+
+    /* store new augeas data */
+    if (aug_save(auginfo.aug) == -1) {
+        AUG_LOG_ERRAUG_GOTO(auginfo.aug, rc, cleanup);
+    }
+
+cleanup:
+    lyd_free_siblings(cur_data);
+    lyd_free_siblings(diff);
+    free(lens_name);
+    free(path);
+    for (i = 0; i < label_count; ++i) {
+        free(label_matches[i]);
+    }
+    free(label_matches);
+    return rc;
 }
 
 static void
@@ -385,6 +729,16 @@ srpds_aug_recover(const struct lys_module *mod, sr_datastore_t ds)
     /* TODO */
 }
 
+/**
+ * @brief Create single YANG node and append to existing data.
+ *
+ * @param[in] schema Schema node of the data node.
+ * @param[in] val_str String value of the data node.
+ * @param[in] parent Optional parent of the data node.
+ * @param[in,out] first First top-level sibling, is updated.
+ * @param[out] node Created node.
+ * @return SR error code.
+ */
 static int
 augds_aug2yang_augnode_create_node(const struct lysc_node *schema, const char *val_str, struct lyd_node *parent,
         struct lyd_node **first, struct lyd_node **node)
@@ -421,6 +775,14 @@ cleanup:
     return rc;
 }
 
+/**
+ * @brief Check whether the extension path node and augeas label node match.
+ *
+ * @param[in] ext_node Extension path node.
+ * @param[in] label_node Augeas label node.
+ * @param[out] node_type Optional type of @p ext_node.
+ * @return Whether the nodes are equal or not.
+ */
 static int
 augds_ext_label_node_equal(const char *ext_node, const char *label_node, enum augds_ext_node_type *node_type)
 {
@@ -471,7 +833,7 @@ static int augds_aug2yang_augnode_r(augeas *aug, const struct augnode *augnodes,
  * @param[in] parent_label Augeas data parent label (absolute path).
  * @param[in,out] label_matches Labels matched for @p parent_label, used ones are freed.
  * @param[in,out] label_count Count of @p label_matches, is updated.
- * @param[in,out] parent YANG data current parent to append to, may be NULL.
+ * @param[in] parent YANG data current parent to append to, may be NULL.
  * @param[in,out] first YANG data first top-level sibling.
  * @return SR error code.
  */
@@ -493,13 +855,13 @@ augds_aug2yang_augnode_labels_r(augeas *aug, const struct augnode *augnodes, uin
 
     for (i = 0; i < augnode_count; ++i) {
         if (augnodes[i].ext_path) {
-            ext_node = augds_get_path_node(augnodes[i].ext_path);
+            ext_node = augds_get_path_node(augnodes[i].ext_path, 0);
 
             /* handle all matching labels */
             j = 0;
             while (j < *label_count) {
                 label = (*label_matches)[j];
-                label_node = augds_get_path_node(label);
+                label_node = augds_get_path_node(label, 0);
                 if (!augds_ext_label_node_equal(ext_node, label_node, &node_type)) {
                     /* not a match */
                     ++j;
@@ -514,9 +876,8 @@ augds_aug2yang_augnode_labels_r(augeas *aug, const struct augnode *augnodes, uin
                     }
 
                     if (!value || !strlen(value)) {
-                        /* no value, skip */
-                        ++j;
-                        continue;
+                        /* no value */
+                        goto label_used;
                     }
                     break;
                 case AUGDS_EXT_NODE_POSITION:
@@ -527,7 +888,7 @@ augds_aug2yang_augnode_labels_r(augeas *aug, const struct augnode *augnodes, uin
                 case AUGDS_EXT_NODE_LABEL_VALUE:
                     /* make sure it matches no following paths */
                     for (k = i + 1; k < augnode_count; ++k) {
-                        if (augds_ext_label_node_equal(augds_get_path_node(augnodes[k].ext_path), label_node, NULL)) {
+                        if (augds_ext_label_node_equal(augds_get_path_node(augnodes[k].ext_path, 0), label_node, NULL)) {
                             break;
                         }
                     }
@@ -553,6 +914,7 @@ augds_aug2yang_augnode_labels_r(augeas *aug, const struct augnode *augnodes, uin
                     goto cleanup;
                 }
 
+label_used:
                 /* label match used, forget it */
                 free(label);
                 --(*label_count);
@@ -591,6 +953,18 @@ cleanup:
     return rc;
 }
 
+/**
+ * @brief Append converted augeas data to YANG data. Convert all data handled by a YANG module
+ * using the context in the augeas handle.
+ *
+ * @param[in] aug Augeas handle.
+ * @param[in] augnodes Array of augnodes to transform.
+ * @param[in] augnode_count Count of @p augnodes.
+ * @param[in] parent_label Augeas data parent label (absolute path).
+ * @param[in] parent YANG data current parent to append to, may be NULL.
+ * @param[in,out] first YANG data first top-level sibling.
+ * @return SR error code.
+ */
 static int
 augds_aug2yang_augnode_r(augeas *aug, const struct augnode *augnodes, uint32_t augnode_count, const char *parent_label,
         struct lyd_node *parent, struct lyd_node **first)
--- tests/test_passwd.c
+++ tests/test_passwd.c
@@ -24,6 +24,7 @@
 #include <dlfcn.h>
 #include <setjmp.h>
 #include <stdarg.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #include <cmocka.h>
@@ -33,7 +34,9 @@
 struct state {
     struct ly_ctx *ctx;
     const struct lys_module *mod;
+    struct lyd_node *data;
     const struct srplg_ds_s *ds_plg;
+    char *str;
 };
 
 static int
@@ -44,6 +47,7 @@ setup_f(void **state)
     st = calloc(1, sizeof *st);
     *state = st;
 
+    sr_log_stderr(SR_LL_WRN);
     //ly_log_level(LY_LLVRB);
 
     /* context */
@@ -71,22 +75,86 @@ teardown_f(void **state)
     ret = st->ds_plg->destroy_cb(st->mod, SR_DS_STARTUP);
 
     /* free */
+    lyd_free_siblings(st->data);
     ly_ctx_destroy(st->ctx);
+    free(st->str);
     free(st);
 
     return ret;
+}
+
+static int
+teardown(void **state)
+{
+    struct state *st = (struct state *)*state;
+
+    lyd_free_siblings(st->data);
+    st->data = NULL;
+    free(st->str);
+    st->str = NULL;
+
+    return 0;
+}
+
+static int
+diff_files(const char *file1, const char *file2, char **output)
+{
+    int wstatus, p[2], r, out_len = 0;
+    char buf[1024];
+
+    *output = strdup("");
+    assert_non_null(*output);
+
+    /* create pipe */
+    pipe(p);
+
+    if (!fork()) {
+        /* child */
+
+        /* output redirect */
+        dup2(p[1], STDOUT_FILENO);
+        close(p[0]);
+        close(p[1]);
+
+        /* exec */
+        execl(AUG_DIFF_EXECUTABLE, AUG_DIFF_EXECUTABLE, file1, file2, (char *)NULL);
+        exit(2);
+    }
+    close(p[1]);
+
+    /* get all output */
+    while ((r = read(p[0], buf, 1023)) > 0) {
+        buf[r] = '\0';
+
+        *output = realloc(*output, out_len + r + 1);
+        snprintf((*output) + out_len, r, buf);
+        out_len += r;
+    }
+    (*output)[out_len] = '\0';
+    assert_int_equal(r, 0);
+
+    /* wait for child */
+    assert_int_not_equal(-1, wait(&wstatus));
+    if (!WIFEXITED(wstatus)) {
+        printf("[ DIFF ERR ] diff process terminated abnormally.\n");
+        return 1;
+    }
+    if (WEXITSTATUS(wstatus) != 1) {
+        printf("[ DIFF ERR ] diff returned unexpected value %d.\n", WEXITSTATUS(wstatus));
+        return 1;
+    }
+
+    return 0;
 }
 
 static void
 test_load(void **state)
 {
     struct state *st = (struct state *)*state;
-    struct lyd_node *data;
     char *str;
 
-    assert_int_equal(SR_ERR_OK, st->ds_plg->load_cb(st->mod, SR_DS_STARTUP, NULL, 0, &data));
-    lyd_print_mem(&str, data, LYD_XML, LYD_PRINT_WITHSIBLINGS);
-    lyd_free_siblings(data);
+    assert_int_equal(SR_ERR_OK, st->ds_plg->load_cb(st->mod, SR_DS_STARTUP, NULL, 0, &st->data));
+    lyd_print_mem(&str, st->data, LYD_XML, LYD_PRINT_WITHSIBLINGS);
 
     assert_string_equal(str,
             "<passwd xmlns=\"aug:passwd\">\n"
@@ -116,7 +184,8 @@ test_load(void **state)
             "    <name>Chrony Daemon</name>\n"
             "    <home>/var/lib/chrony</home>\n"
             "    <shell>/bin/false</shell>\n"
-            "  </entry>\n""  <entry>\n"
+            "  </entry>\n"
+            "  <entry>\n"
             "    <username>man</username>\n"
             "    <password>x</password>\n"
             "    <uid>13</uid>\n"
@@ -180,12 +249,40 @@ test_load(void **state)
     free(str);
 }
 
+static void
+test_store_add(void **state)
+{
+    struct state *st = (struct state *)*state;
+    struct lyd_node *user;
+
+    /* load current data */
+    assert_int_equal(SR_ERR_OK, st->ds_plg->load_cb(st->mod, SR_DS_STARTUP, NULL, 0, &st->data));
+
+    /* add a user */
+    assert_int_equal(LY_SUCCESS, lyd_new_path(st->data, NULL, "entry[username='admin']/password", "x", 0, &user));
+    assert_int_equal(LY_SUCCESS, lyd_new_path(user, NULL, "uid", "2000", 0, NULL));
+    assert_int_equal(LY_SUCCESS, lyd_new_path(user, NULL, "gid", "200", 0, NULL));
+    assert_int_equal(LY_SUCCESS, lyd_new_path(user, NULL, "name", "The Admin", 0, NULL));
+    assert_int_equal(LY_SUCCESS, lyd_new_path(user, NULL, "home", "/home/admin", 0, NULL));
+    assert_int_equal(LY_SUCCESS, lyd_new_path(user, NULL, "shell", "/bin/bash", 0, NULL));
+
+    /* store new data */
+    assert_int_equal(SR_ERR_OK, st->ds_plg->store_cb(st->mod, SR_DS_STARTUP, st->data));
+
+    /* get diff */
+    assert_int_equal(0, diff_files(AUG_TEST_INPUT_FILE, AUG_TEST_INPUT_FILE ".augnew", &st->str));
+    assert_string_equal(st->str,
+            "13a14\n"
+            "> admin:x:2000:200:The Admin:/home/admin:/bin/bash");
+}
+
 int
 main(void)
 {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test_setup_teardown(test_load, setup_f, teardown_f),
+        cmocka_unit_test_teardown(test_load, teardown),
+        cmocka_unit_test_teardown(test_store_add, teardown),
     };
 
-    return cmocka_run_group_tests(tests, NULL, NULL);
+    return cmocka_run_group_tests(tests, setup_f, teardown_f);
 }
