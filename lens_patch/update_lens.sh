2e17e2e4cccd0b4647b815ac480229458c01d31d
Michal Vasko <mvasko@cesnet.cz>
2022-04-11 09:48:13 +0200

    lens patch UPDATE support for no patches

--- lens_patch/update_lens.sh
+++ lens_patch/update_lens.sh
@@ -12,7 +12,7 @@ AUGEAS_LENS_DIR=$2
 
 wget -O augeas.tar.gz https://github.com/hercules-team/augeas/archive/master.tar.gz 2> /dev/null
 tar -xvf augeas.tar.gz augeas-master/lenses 1> /dev/null
-for LENS_P in `ls $LENS_PATCH_DIR/*.patch`; do
+for LENS_P in `find $LENS_PATCH_DIR -name '*.patch'`; do
     LENS=`echo $LENS_P | grep -o '[^/]*$' | grep -o '^[^.]*\\.[^.]*'`
     patch augeas-master/lenses/$LENS $LENS_P
 done
