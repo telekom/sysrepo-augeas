72955e11b5576a9acec7c93923c092bba83115b3
Michal Vasko <mvasko@cesnet.cz>
2022-04-26 14:40:04 +0200

    srds augeas FEATURE darkice lens support

---
+++ tests/test_darkice.c
@@ -0,0 +1,211 @@
+/**
+ * @file test_darkice.c
+ * @author Michal Vasko <mvasko@cesnet.cz>
+ * @brief cmdline SR DS plugin test
+ *
+ * @copyright
+ * Copyright (c) 2022 Deutsche Telekom AG.
+ * Copyright (c) 2022 CESNET, z.s.p.o.
+ *
+ * This source code is licensed under BSD 3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/BSD-3-Clause
+ */
+
+#include "tconfig.h"
+
+/* augeas SR DS plugin */
+#define AUG_TEST_INPUT_FILES AUG_CONFIG_FILES_DIR "/darkice"
+#include "srds_augeas.c"
+#include "srdsa_init.c"
+#include "srdsa_load.c"
+#include "srdsa_store.c"
+#include "srdsa_common.c"
+
+#include <assert.h>
+#include <dlfcn.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#include <cmocka.h>
+#include <libyang/libyang.h>
+#include <sysrepo/plugins_datastore.h>
+
+#define AUG_TEST_MODULE "darkice"
+
+static int
+setup_f(void **state)
+{
+    return tsetup_glob(state, AUG_TEST_MODULE, &srpds__, AUG_TEST_INPUT_FILES);
+}
+
+static void
+test_load(void **state)
+{
+    struct tstate *st = (struct tstate *)*state;
+    char *str;
+
+    assert_int_equal(SR_ERR_OK, st->ds_plg->load_cb(st->mod, SR_DS_STARTUP, NULL, 0, &st->data));
+    lyd_print_mem(&str, st->data, LYD_XML, LYD_PRINT_WITHSIBLINGS);
+
+    assert_string_equal(str,
+            "<" AUG_TEST_MODULE " xmlns=\"aug:" AUG_TEST_MODULE "\">\n"
+            "  <config-file>" AUG_CONFIG_FILES_DIR "/" AUG_TEST_MODULE "</config-file>\n"
+            "  <record-list>\n"
+            "    <_id>1</_id>\n"
+            "    <record>\n"
+            "      <value>general</value>\n"
+            "      <entry-re-list>\n"
+            "        <_id>1</_id>\n"
+            "        <entry-re>\n"
+            "          <entry-re>duration</entry-re>\n"
+            "          <value>0</value>\n"
+            "        </entry-re>\n"
+            "      </entry-re-list>\n"
+            "      <entry-re-list>\n"
+            "        <_id>2</_id>\n"
+            "        <entry-re>\n"
+            "          <entry-re>bufferSecs</entry-re>\n"
+            "          <value>5</value>\n"
+            "        </entry-re>\n"
+            "      </entry-re-list>\n"
+            "    </record>\n"
+            "  </record-list>\n"
+            "  <record-list>\n"
+            "    <_id>2</_id>\n"
+            "    <record>\n"
+            "      <value>icecast2-0</value>\n"
+            "      <entry-re-list>\n"
+            "        <_id>1</_id>\n"
+            "        <entry-re>\n"
+            "          <entry-re>bitrateMode</entry-re>\n"
+            "          <value>cbr</value>\n"
+            "        </entry-re>\n"
+            "      </entry-re-list>\n"
+            "      <entry-re-list>\n"
+            "        <_id>2</_id>\n"
+            "        <entry-re>\n"
+            "          <entry-re>format</entry-re>\n"
+            "          <value>vorbis</value>\n"
+            "        </entry-re>\n"
+            "      </entry-re-list>\n"
+            "    </record>\n"
+            "  </record-list>\n"
+            "</" AUG_TEST_MODULE ">\n");
+    free(str);
+}
+
+static void
+test_store_add(void **state)
+{
+    struct tstate *st = (struct tstate *)*state;
+    struct lyd_node *entries, *node;
+
+    /* load current data */
+    assert_int_equal(SR_ERR_OK, st->ds_plg->load_cb(st->mod, SR_DS_STARTUP, NULL, 0, &st->data));
+
+    /* add some new list instances */
+    assert_int_equal(LY_SUCCESS, lyd_new_path(st->data, NULL, "record-list[_id='3']/record/value", "my-section", 0, &entries));
+    assert_int_equal(LY_SUCCESS, lyd_new_path(st->data, NULL, "record-list[_id='3']/record/entry-re-list[_id='1']/"
+            "entry-re/entry-re", "logging", 0, NULL));
+    assert_int_equal(LY_SUCCESS, lyd_new_path(st->data, NULL, "record-list[_id='3']/record/entry-re-list[_id='1']/"
+            "entry-re/value", "none", 0, NULL));
+    assert_int_equal(LY_SUCCESS, lyd_find_path(st->data, "record-list[_id='1']", 0, &node));
+    assert_int_equal(LY_SUCCESS, lyd_insert_after(node, entries));
+
+    assert_int_equal(LY_SUCCESS, lyd_new_path(st->data, NULL, "record-list[_id='1']/record/entry-re-list[_id='3']/"
+            "entry-re/entry-re", "foo", 0, &entries));
+    assert_int_equal(LY_SUCCESS, lyd_new_path(st->data, NULL, "record-list[_id='1']/record/entry-re-list[_id='3']/"
+            "entry-re/value", "bar", 0, NULL));
+    assert_int_equal(LY_SUCCESS, lyd_find_path(st->data, "record-list[_id='1']/record/entry-re-list[_id='1']", 0, &node));
+    assert_int_equal(LY_SUCCESS, lyd_insert_after(node, entries));
+
+    /* store new data */
+    assert_int_equal(SR_ERR_OK, st->ds_plg->store_cb(st->mod, SR_DS_STARTUP, st->data));
+
+    /* diff */
+    assert_int_equal(0, tdiff_files(state,
+            "4a5\n"
+            "> foo=bar\n"
+            "6a8,9\n"
+            "> [my-section]\n"
+            "> logging=none\n"));
+}
+
+static void
+test_store_modify(void **state)
+{
+    struct tstate *st = (struct tstate *)*state;
+
+    /* load current data */
+    assert_int_equal(SR_ERR_OK, st->ds_plg->load_cb(st->mod, SR_DS_STARTUP, NULL, 0, &st->data));
+
+    /* modify some values */
+    assert_int_equal(LY_SUCCESS, lyd_new_path(st->data, NULL, "record-list[_id='2']/record/value", "icecast5-0",
+            LYD_NEW_PATH_UPDATE, NULL));
+    assert_int_equal(LY_SUCCESS, lyd_new_path(st->data, NULL, "record-list[_id='1']/record/entry-re-list[_id='1']/"
+            "entry-re/entry-re", "length", LYD_NEW_PATH_UPDATE, NULL));
+    assert_int_equal(LY_SUCCESS, lyd_new_path(st->data, NULL, "record-list[_id='1']/record/entry-re-list[_id='2']/"
+            "entry-re/value", "10", LYD_NEW_PATH_UPDATE, NULL));
+
+    /* store new data */
+    assert_int_equal(SR_ERR_OK, st->ds_plg->store_cb(st->mod, SR_DS_STARTUP, st->data));
+
+    /* diff */
+    assert_int_equal(0, tdiff_files(state,
+            "4,5c4,5\n"
+            "< duration        = 0\n"
+            "< bufferSecs      = 5         # size of internal slip buffer, in seconds\n"
+            "---\n"
+            "> length=0\n"
+            "> bufferSecs      = 10         # size of internal slip buffer, in seconds\n"
+            "7c7\n"
+            "< [icecast2-0]\n"
+            "---\n"
+            "> [icecast5-0]\n"));
+}
+
+static void
+test_store_remove(void **state)
+{
+    struct tstate *st = (struct tstate *)*state;
+    struct lyd_node *node;
+
+    /* load current data */
+    assert_int_equal(SR_ERR_OK, st->ds_plg->load_cb(st->mod, SR_DS_STARTUP, NULL, 0, &st->data));
+
+    /* remove list values */
+    assert_int_equal(LY_SUCCESS, lyd_find_path(st->data, "record-list[_id='1']", 0, &node));
+    lyd_free_tree(node);
+    assert_int_equal(LY_SUCCESS, lyd_find_path(st->data, "record-list[_id='2']/record/entry-re-list[_id='2']", 0, &node));
+    lyd_free_tree(node);
+
+    /* store new data */
+    assert_int_equal(SR_ERR_OK, st->ds_plg->store_cb(st->mod, SR_DS_STARTUP, st->data));
+
+    /* diff */
+    assert_int_equal(0, tdiff_files(state,
+            "3,6d2\n"
+            "< [general]\n"
+            "< duration        = 0\n"
+            "< bufferSecs      = 5         # size of internal slip buffer, in seconds\n"
+            "< \n"
+            "9d4\n"
+            "< format=vorbis\n"));
+}
+
+int
+main(void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test_teardown(test_load, tteardown),
+        cmocka_unit_test_teardown(test_store_add, tteardown),
+        cmocka_unit_test_teardown(test_store_modify, tteardown),
+        cmocka_unit_test_teardown(test_store_remove, tteardown),
+    };
+
+    return cmocka_run_group_tests(tests, setup_f, tteardown_glob);
+}
