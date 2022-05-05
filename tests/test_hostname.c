d67826ffd40be0b9504afe73039b0ede1a9369da
Michal Vasko <mvasko@cesnet.cz>
2022-05-05 13:29:22 +0200

    srds augeas FEATURE hostname lens support

---
+++ tests/test_hostname.c
@@ -0,0 +1,94 @@
+/**
+ * @file test_hostname.c
+ * @author Michal Vasko <mvasko@cesnet.cz>
+ * @brief hostname SR DS plugin test
+ *
+ * @copyright
+ * Copyright (c) 2022 Deutsche Telekom AG.
+ * Copyright (c) 2022 CESNET, z.s.p.o.
+ *
+ * This source code is licensed under BSD 3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/BSD-3-Clause
+ */
+
+#include "tconfig.h"
+
+/* augeas SR DS plugin */
+#define AUG_TEST_INPUT_FILES AUG_CONFIG_FILES_DIR "/hostname"
+#include "srds_augeas.c"
+#include "srdsa_init.c"
+#include "srdsa_load.c"
+#include "srdsa_store.c"
+#include "srdsa_common.c"
+
+#include <assert.h>
+#include <dlfcn.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#include <cmocka.h>
+#include <libyang/libyang.h>
+#include <sysrepo/plugins_datastore.h>
+
+#define AUG_TEST_MODULE "hostname"
+
+static int
+setup_f(void **state)
+{
+    return tsetup_glob(state, AUG_TEST_MODULE, &srpds__, AUG_TEST_INPUT_FILES);
+}
+
+static void
+test_load(void **state)
+{
+    struct tstate *st = (struct tstate *)*state;
+    char *str;
+
+    assert_int_equal(SR_ERR_OK, st->ds_plg->load_cb(st->mod, SR_DS_STARTUP, NULL, 0, &st->data));
+    lyd_print_mem(&str, st->data, LYD_XML, LYD_PRINT_WITHSIBLINGS);
+
+    assert_string_equal(str,
+            "<" AUG_TEST_MODULE " xmlns=\"aug:" AUG_TEST_MODULE "\">\n"
+            "  <config-file>" AUG_CONFIG_FILES_DIR "/" AUG_TEST_MODULE "</config-file>\n"
+            "  <hostname>local.localnet</hostname>\n"
+            "</" AUG_TEST_MODULE ">\n");
+    free(str);
+}
+
+static void
+test_store_modify(void **state)
+{
+    struct tstate *st = (struct tstate *)*state;
+
+    /* load current data */
+    assert_int_equal(SR_ERR_OK, st->ds_plg->load_cb(st->mod, SR_DS_STARTUP, NULL, 0, &st->data));
+
+    /* modify some values */
+    assert_int_equal(LY_SUCCESS, lyd_new_path(st->data, NULL, "hostname", "local.localnet.cz",
+            LYD_NEW_PATH_UPDATE, NULL));
+
+    /* store new data */
+    assert_int_equal(SR_ERR_OK, st->ds_plg->store_cb(st->mod, SR_DS_STARTUP, st->data));
+
+    /* diff */
+    assert_int_equal(0, tdiff_files(state,
+            "1c1\n"
+            "< local.localnet\n"
+            "---\n"
+            "> local.localnet.cz\n"));
+}
+
+int
+main(void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test_teardown(test_load, tteardown),
+        cmocka_unit_test_teardown(test_store_modify, tteardown),
+    };
+
+    return cmocka_run_group_tests(tests, setup_f, tteardown_glob);
+}
